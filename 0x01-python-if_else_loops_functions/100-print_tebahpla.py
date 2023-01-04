5b6de3dd15746e211deb059e498d11b2f8058f2a
Luis Saiete <luistsaiete@gmail.com>
2023-01-04 09:37:39 +0200

    Use only one print

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,7 @@
 for i in range(122, 96, -1):
     if i % 2 == 0:
         ch = chr(i)
-        print(ch, end='')
     else:
         ch = chr(i-32)
-        print(ch, end='')
+
+    print(ch, end='')
