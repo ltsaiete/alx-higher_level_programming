230354d10b14b56dd1926cbfa66ff7cbbea56f85
Luis Saiete <luistsaiete@gmail.com>
2023-01-04 08:19:17 +0200

    Fix tasks 5 and 6

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(99):
-    print(f'{i:02d}', end=', ')
+    print('{:02d}'.format(i), end=', ')
 
-print('99')
+print('{:02d}'.format(99))
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(10):
     for j in range(10):
         if j > i:
             if i == 8:
-                print(f'{i}{j}')
+                print('{}{}'.format(i, j))
                 break
-            print(f'{i}{j}', end=", ")
+            print('{}{}'.format(i, j), end=", ")
