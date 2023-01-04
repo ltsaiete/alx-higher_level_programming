5366e0e976e4dfdc95230dbb312cb38d2c9f88ce
Luis Saiete <luistsaiete@gmail.com>
2023-01-04 09:35:18 +0200

    Remove format

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
     if i % 2 == 0:
-        print(f'{i:c}', end='')
+        ch = chr(i)
+        print(ch, end='')
     else:
-        print(f'{i-32:c}', end='')
+        ch = chr(i-32)
+        print(ch, end='')
