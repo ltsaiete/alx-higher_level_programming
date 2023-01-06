292f807a67f939cb74eafede96e821e4911f8cee
Luis Saiete <luistsaiete@gmail.com>
2023-01-06 14:03:07 +0200

    Complete task 5

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+if __name__ == '__main__':
+    import importlib.util
+    import importlib.machinery
+
+    bytecode_path = './hidden_4.pyc'
+    loader = importlib.machinery.SourceFileLoader('hidden_4', bytecode_path)
+    spec = importlib.util.spec_from_loader('hidden_4', loader)
+    module = importlib.util.module_from_spec(spec)
+
+    spec.loader.exec_module(module)
