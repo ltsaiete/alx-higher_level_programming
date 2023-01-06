64852310950879e2b185330c6c261c68a01f357a
Luis Saiete <luistsaiete@gmail.com>
2023-01-06 13:01:58 +0200

    Complete task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    count = len(sys.argv)
+    if count == 1:
+        print('0 arguments.')
+    elif count == 2:
+        print('1 argument:')
+    else:
+        print(f'{count - 1} arguments:')
+
+    for i in range(1, count):
+        print(f'{i}: {sys.argv[i]}')
