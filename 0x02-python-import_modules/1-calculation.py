d7fe1ce0b1a37f5868ebd6ab9a34e8f25274a9d6
Luis Saiete <luistsaiete@gmail.com>
2023-01-06 12:36:59 +0200

    Format division with :d

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
     print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
     print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
-    print('{:d} / {:d} = {:f}'.format(a, b, div(a, b)))
+    print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
