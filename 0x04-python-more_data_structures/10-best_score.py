7018926e87545e2cb4370c06a8a28b9cc6d85503
Luis Saiete <luistsaiete@gmail.com>
2023-01-12 00:54:52 +0200

    Revert

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,8 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
 
-    bigKey, bigValue = None, a_dictionary[list(a_dictionary)[0]]
+    bigKey, bigValue = list(a_dictionary)[
+        0], a_dictionary[list(a_dictionary)[0]]
 
     for k, v in a_dictionary.items():
         if v > bigValue:
