611ed12ec86b92816707bd2abea34a4415c5c179
Luis Saiete <luistsaiete@gmail.com>
2023-01-24 23:25:48 +0200

    Add advanced tasks

--- 0x05-python-exceptions/2-main.py
+++ 0x05-python-exceptions/2-main.py
@@ -2,9 +2,9 @@
 safe_print_list_integers = \
     __import__('2-safe_print_list_integers').safe_print_list_integers
 
-my_list = [1, 2, 3, 4, 5]
+my_list = [1, 2, 3, 4]
 
-nb_print = safe_print_list_integers(my_list, 2)
+nb_print = safe_print_list_integers(my_list, len(my_list) + 4)
 print("nb_print: {:d}".format(nb_print))
 
 my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
