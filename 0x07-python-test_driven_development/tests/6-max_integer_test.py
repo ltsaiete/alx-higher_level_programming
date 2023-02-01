a9ee00a78ac17a657313597842aa119ee9c3727c
Luis Saiete <luistsaiete@gmail.com>
2023-02-01 18:07:19 +0200

    Add task 6

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class MaxIntegerTest(unittest.TestCase):
+    def test_max(self):
+        self.assertEqual(5, max_integer([1, 2, 3, 4, 5]))
+        self.assertIsNone(max_integer())
+
+    def test_type(self):
+        self.assertRaises(TypeError, max_integer, [1, 2, 'ALX'])
