568a264284a23c516cde9cb093e0da7be9c5a3a3
Luis Saiete <luistsaiete@gmail.com>
2023-04-28 15:59:09 +0200

    Fix task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -58,7 +58,7 @@ class Square:
         if (self.__size == 0):
             print()
         for _ in range(self.__size):
+            print(' ' * self.__position[0], end='')
             for _ in range(self.__size):
-                print(' ' * self.__position[0], end='')
                 print('#', end='')
             print()
