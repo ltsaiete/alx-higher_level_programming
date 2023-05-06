5d1e44f94ce94b762d8f5206cc632018da43a3e7
Luis Saiete <luistsaiete@gmail.com>
2023-05-06 11:06:28 +0200

    Fix task 19

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,19 +1,3 @@
 #!/usr/bin/python3
-"""
-This is a simple module and it only has
-one function called copy_list
-"""
-
-
 def copy_list(l):
-    """
-    This function  returns a copy of a list.
-
-    Args:
-        l(list): The list to copy
-
-    Returns:
-        a copy of l
-    """
-
     return l[:]
