8a93db9135c1610e6966d6c4706907feccb4f100
Luis Saiete <luistsaiete@gmail.com>
2023-05-06 12:40:05 +0200

    fix task 100

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    if not hasattr(magic_string, "count"):
-        magic_string.count = 0
-    magic_string.count += 1
+    setattr(magic_string, 'count', hasattr(magic_string, 'count') and getattr(magic_string, 'count')+1 or 0)
     return ", ".join(["BestSchool"] * magic_string.count)
