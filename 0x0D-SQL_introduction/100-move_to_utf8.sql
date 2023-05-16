8ca5705a888704eb5e8a310a2cfda9f40b6384e2
Luis Saiete <luistsaiete@gmail.com>
2023-05-17 00:14:01 +0200

    Fix task 100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -9,8 +9,3 @@ USE hbtn_0c_0;
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
-
-ALTER TABLE first_table
-MODIFY COLUMN name
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
