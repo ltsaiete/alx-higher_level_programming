00123ba30ea8cf11d9289ba09f04a0ef3183de7a
Luis Saiete <luistsaiete@gmail.com>
2023-05-17 00:10:06 +0200

    Fix task 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,3 @@
 -- prints the full description of the table first_table from the
 -- database hbtn_0c_0 in your MySQL server.
-SELECT column_name, data_type, character_maximum_length
-FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0'
-  AND table_name = 'first_table';
+SHOW CREATE TABLE `first_table`;
