59a9e7ffdef0ac8cbda4e57c07b6b8b71a3edcc0
Luis Saiete <luistsaiete@gmail.com>
2023-05-18 01:54:01 +0200

    Update tasks 19 and 20

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
 -- lists all shows from hbtn_0d_tvshows_rate by their rating.
 -- Each record should display: tv_shows.title - rating sum
 -- Results must be sorted in descending order by the rating
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) as rating
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
 JOIN tv_show_ratings
 ON tv_show_ratings.show_id = tv_shows.id
--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,7 @@
 -- lists all genres in the database hbtn_0d_tvshows_rate by their rating.
 -- Each record should display: tv_genres.name - rating sum
 -- Results must be sorted in descending order by their rating
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) as rating
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
