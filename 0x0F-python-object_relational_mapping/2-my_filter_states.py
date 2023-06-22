2eb5fc541b632925828f1be1ef6e0ebaaa5aa1bb
Luis Saiete <luistsaiete@gmail.com>
2023-06-22 23:00:40 +0200

    Fix pycodestyle validation

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -18,7 +18,10 @@ if __name__ == '__main__':
 
     cur = db.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE name = '{}' ORDER BY id".format(sys.argv[4]))
+        """SELECT * FROM states
+        WHERE name = '{}'
+        ORDER BY id""".format(sys.argv[4])
+    )
 
     rows = cur.fetchall()
     for row in rows:
