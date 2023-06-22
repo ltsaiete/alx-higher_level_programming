a209b9445e8283090f0cc3ca0237ab8255c85e7e
Luis Saiete <luistsaiete@gmail.com>
2023-06-22 23:09:13 +0200

    Print Nothing if table is empty

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -15,4 +15,7 @@ if __name__ == '__main__':
     session = Session(bind=engine)
 
     state = session.query(State).first()
-    print(f'{state.id}: {state.name}')
+    if state:
+        print(f'{state.id}: {state.name}')
+    else:
+        print('Nothing')
