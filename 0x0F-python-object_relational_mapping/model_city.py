6fc13f573653bfc8af5f8c848ecbc21a3b2d5a21
Luis Saiete <luistsaiete@gmail.com>
2023-06-22 23:31:05 +0200

    Fix Base declaration in City class
    It should've been imported from State

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -7,8 +7,7 @@ an instance Base = declarative_base()
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from model_state import Base
 
 
 class City(Base):
--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -8,8 +8,7 @@ an instance Base = declarative_base()
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-
-Base = declarative_base()
+from relationship_state import Base
 
 
 class City(Base):
